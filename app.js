3f049304a463a12abc5d907343c6f3ef79c37a1d
Prabhav-Sharma <prabhav.ps8@gmail.com>
2022-01-10 12:21:14 +0530

    Feat: ES6
    Code updated to ES6 version

--- app.js
+++ app.js
@@ -2,14 +2,30 @@ var btnTranslate = document.querySelector("#btn-translate");
 var txtTranslate = document.querySelector("#txt-input");
 var outputBox = document.querySelector("#output-box");
 
-btnTranslate.addEventListener("click", clicked);
 
-function getUrl(text) {
+/* ES5 version 
+  function getUrl(text) {
     var url = "https://api.funtranslations.com/translate/minion.json"
     return url + "?text=" + text
 }
+*/
 
-function clicked() {
-    var userInput = txtTranslate.value;
-    fetch(getUrl(userInput)).then(response => response.json()).then(json => outputBox.innerText = json.contents.translated);
-}
+//ES6+ version 
+const getUrl = text=> `https://api.funtranslations.com/translate/minion.json?text=${text}`;
+
+// ES5 Version
+// function clicked() {
+//     var userInput = txtTranslate.value;
+//     fetch(getUrl(userInput)).then(response => response.json()).then(json => outputBox.innerText = json.contents.translated);
+// }
+
+
+// ES6+ version
+const clicked = _=>{
+let userInput = txtTranslate.value;
+fetch(getUrl(userInput)).then(response => response.json()).then(json => outputBox.innerText = json.contents.translated);
+};
+
+btnTranslate.addEventListener("click", clicked);
+
+
